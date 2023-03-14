70bb1b5c0a637a0c15c292538bab4b4d935f96e1
JosephJandres <109707096+KouhaiTostado@users.noreply.github.com>
2023-03-14 10:56:36 -0600

    Add files via upload

---
+++ practica 4.py
@@ -0,0 +1,5 @@
+número1 = float(input("Ingresa el primer número:"))
+número2 = float(input("Ingresa el segundo número:"))
+print ("Tu Resultado es:")
+resultado = número1 * número2
+print (resultado)
