2b16bc613b4f066a063c0e9b4eda92ef785786c3
fernansaurio <108437945+fernansaurio@users.noreply.github.com>
2023-03-16 21:58:42 -0600

    Delete Practica 7.py

--- Practica 7.py
+++
@@ -1,8 +0,0 @@
-segundos = int(input("Inserta la cantidad de segundos: "))
-horas =  segundos // 3600
-segundos_faltantes = segundos % 3600
-minutos = segundos_faltantes // 60
-segundos_restantes_final = segundos_faltantes % 60
-
-print(f"{segundos}segundos equivalen a:")
-print(f"{horas} horas, {minutos} minutos y {segundos_restantes_final} segundos")
