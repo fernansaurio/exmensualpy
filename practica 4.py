fe6916db2a06d8d5ae30b1c02dac6b4250a5d2b0
fernansaurio <108437945+fernansaurio@users.noreply.github.com>
2023-03-16 21:59:17 -0600

    Delete practica 4.py

--- practica 4.py
+++
@@ -1,5 +0,0 @@
-número1 = float(input("Ingresa el primer número:"))
-número2 = float(input("Ingresa el segundo número:"))
-print ("Tu Resultado es:")
-resultado = número1 * número2
-print (resultado)
