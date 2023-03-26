5577fe8ff102c894089a735f02de4199053d5b00
fernansaurio <108437945+fernansaurio@users.noreply.github.com>
2023-03-26 10:07:25 -0600

    Add files via upload

---
+++ Practica11.py
@@ -0,0 +1,10 @@
+metros_cubicos = int(input("Ingrese la cantidad de metros cúbicos consumidos: "))
+
+if metros_cubicos <= 18:
+    cantidad_pagar = 6
+elif metros_cubicos <= 28:
+    cantidad_pagar = 6 + (metros_cubicos - 18) * 0.45
+else:
+    cantidad_pagar = 6 + 4.5 + (metros_cubicos - 28) * 0.65
+
+print("La cantidad a pagar es de:", cantidad_pagar, "dólares")
